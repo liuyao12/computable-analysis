25a7497adcb02ac2f9e95457d24f42e51f912476
Yao Liu <liuyao@gmail.com>
2026-08-13 13:11:34 -0700

    Import finite prime-interval interface

--- ComputableAnalysis.lean
+++ ComputableAnalysis.lean
@@ -12,6 +12,7 @@ import ComputableAnalysis.ArctanGeomInverseData
 import ComputableAnalysis.ArctanRectanglePi
 import ComputableAnalysis.CauchyPi
 import ComputableAnalysis.Basic
+import ComputableAnalysis.FinitePrimeIntervalInterface
 import ComputableAnalysis.FiniteInverseSearchInterface
 import ComputableAnalysis.FiniteMonotoneSequenceInterface
 import ComputableAnalysis.FiniteEuclideanGcdInterface
