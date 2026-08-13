35ee6eac261c62f8c294dbffa9faddb1526084de
Yao Liu <liuyao@gmail.com>
2026-08-13 13:20:21 -0700

    Import finite law-of-cosines interface

--- ComputableAnalysis.lean
+++ ComputableAnalysis.lean
@@ -12,6 +12,7 @@ import ComputableAnalysis.ArctanGeomInverseData
 import ComputableAnalysis.ArctanRectanglePi
 import ComputableAnalysis.CauchyPi
 import ComputableAnalysis.Basic
+import ComputableAnalysis.FiniteLawOfCosinesInterface
 import ComputableAnalysis.FinitePickInterface
 import ComputableAnalysis.FiniteHarmonicGrowthInterface
 import ComputableAnalysis.FinitePrimeIntervalInterface
