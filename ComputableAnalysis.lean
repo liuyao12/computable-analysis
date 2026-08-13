ae9552df6f022ee2ef4af13e6fc060cd816b074e
Yao Liu <liuyao@gmail.com>
2026-08-13 12:09:32 -0700

    Import shared anchor theorem

--- ComputableAnalysis.lean
+++ ComputableAnalysis.lean
@@ -12,6 +12,7 @@ import ComputableAnalysis.ArctanGeomInverseData
 import ComputableAnalysis.ArctanRectanglePi
 import ComputableAnalysis.CauchyPi
 import ComputableAnalysis.Basic
+import ComputableAnalysis.SharedRationalAnchor
 import ComputableAnalysis.Basel
 import ComputableAnalysis.BaselFiniteStrengthening
 import ComputableAnalysis.FiniteBaselCertificate
