ea368c14260a8de66f58c6e96fdee3775f207190
Yao Liu <liuyao@gmail.com>
2026-08-13 13:27:03 -0700

    Import finite triangle-orientation interface

--- ComputableAnalysis.lean
+++ ComputableAnalysis.lean
@@ -12,6 +12,7 @@ import ComputableAnalysis.ArctanGeomInverseData
 import ComputableAnalysis.ArctanRectanglePi
 import ComputableAnalysis.CauchyPi
 import ComputableAnalysis.Basic
+import ComputableAnalysis.FiniteTriangleOrientationInterface
 import ComputableAnalysis.FiniteAlternatingSeriesInterface
 import ComputableAnalysis.FiniteDeMoivreInterface
 import ComputableAnalysis.FiniteLawOfCosinesInterface
