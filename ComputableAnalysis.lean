b1f7de66b05244fc3331323f248db40953c79a08
Yao Liu <liuyao@gmail.com>
2026-08-13 13:22:06 -0700

    Import finite De Moivre interface

--- ComputableAnalysis.lean
+++ ComputableAnalysis.lean
@@ -12,6 +12,7 @@ import ComputableAnalysis.ArctanGeomInverseData
 import ComputableAnalysis.ArctanRectanglePi
 import ComputableAnalysis.CauchyPi
 import ComputableAnalysis.Basic
+import ComputableAnalysis.FiniteDeMoivreInterface
 import ComputableAnalysis.FiniteLawOfCosinesInterface
 import ComputableAnalysis.FinitePickInterface
 import ComputableAnalysis.FiniteHarmonicGrowthInterface
