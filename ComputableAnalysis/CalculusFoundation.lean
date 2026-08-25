489c269f48f24f410d8d8794f395a6fab1532178
liuyao12 <liuyao@gmail.com>
2026-08-24 20:53:12 -0700

    Expose stable rotation derivative certificates

--- ComputableAnalysis/CalculusFoundation.lean
+++ ComputableAnalysis/CalculusFoundation.lean
@@ -14,6 +14,7 @@ import ComputableAnalysis.SinPiTransportSubgoals
 import ComputableAnalysis.SinPiTransportAdapter
 import ComputableAnalysis.SinPiSquareFTC
 import ComputableAnalysis.TangentPullbackEffectiveFTC
+import ComputableAnalysis.StableRotationDerivative
 import ComputableAnalysis.EffectiveFTCPortfolio
 import ComputableAnalysis.MonotonicityConvexity
 import ComputableAnalysis.PowerSeries
